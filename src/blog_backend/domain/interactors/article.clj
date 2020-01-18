0b127d6508e1d915f33f6baaf0e961b286a293ab
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-01-18 13:35:35 -0500

    prevent using an alias for article repository

--- src/blog_backend/domain/interactors/article.clj
+++ src/blog_backend/domain/interactors/article.clj
@@ -1,9 +1,9 @@
 (ns blog-backend.domain.interactors.article
   (:refer-clojure :exclude [update find])
-  (:require [blog-backend.domain.repository.article :as interactor :refer [save, modify]]))
+  (:require [blog-backend.domain.repository.article :refer [save, modify]]))
 
 (defn create [article repository]
-  (interactor/save repository article))
+  (save repository article))
 
 (defn update [article repository]
-  (interactor/modify repository article))
+  (modify repository article))
