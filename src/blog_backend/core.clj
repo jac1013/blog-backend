937b293a646fac26d2c393b352ce001eac74d130
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-01-18 12:22:30 -0500

    use dependecy inversion for create article

--- src/blog_backend/core.clj
+++ src/blog_backend/core.clj
@@ -1,7 +1,8 @@
 (ns blog-backend.core
   (:gen-class)
   (:refer-clojure :exclude [update])
-  (:require [blog-backend.domain.interactors.article :refer [create update]]))
+  (:require [blog-backend.domain.interactors.article :refer [create update]])
+  (:import (blog_backend.domain.repository.article ArticleRepo)))
 
 
 ;(defn new_article_repo [] (fn [] (ArticleRepo.)))
@@ -9,6 +10,6 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (create {:id 1}))
+  (println (create {:id 1} (ArticleRepo.)))
   (println (update {:id 2})))
 
--- src/blog_backend/domain/interactors/article.clj
+++ src/blog_backend/domain/interactors/article.clj
@@ -3,9 +3,8 @@
   (:require [blog-backend.domain.repository.article :as interactor :refer [save, modify]])
   (:import (blog_backend.domain.repository.article ArticleRepo)))
 
-(defn create [article]
-  (let [ar (ArticleRepo.)]
-        (interactor/save ar article)))
+(defn create [article repository]
+  (interactor/save repository article))
 
 (defn update [article]
   (let [ar (ArticleRepo.)]
