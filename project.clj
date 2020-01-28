b024defecd24725b20b7a4173ffc4021aac11765
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-01-28 06:55:15 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://to-be-defined.com"
   :license {:name "MIT"
             :url "https://en.wikipedia.org/wiki/MIT_License"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ring/ring-core "1.6.3"]
-                 [ring/ring-jetty-adapter "1.6.3"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring/ring-core "1.8.0"]
+                 [ring/ring-jetty-adapter "1.8.0"]
                  [ring/ring-json "0.5.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [com.novemberain/validateur "2.5.0"]]
+                 [com.novemberain/validateur "2.6.0"]]
   :main dev.codecarver.core
   :aot [dev.codecarver.core]
   :target-path "target/%s"
