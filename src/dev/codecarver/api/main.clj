f724bb26e58884a293c94a2151d3a5400f2e786d
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-01-28 20:53:20 -0500

    separate artitle routes definition from main

--- src/dev/codecarver/api/main.clj
+++ src/dev/codecarver/api/main.clj
@@ -1,27 +1,14 @@
 (ns dev.codecarver.api.main
   (:use ring.adapter.jetty)
-  (:require [compojure.core :refer :all]
+  (:require [compojure.core :refer [defroutes wrap-routes GET routes]]
             [compojure.route :as route])
-  (:require [ring.middleware.defaults :refer :all])
-  (:require [ring.middleware.params :refer [wrap-params]])
-  (:require [ring.middleware.json :refer [wrap-json-response]])
-  (:require [validateur.validation :refer :all])
-  (:require [dev.codecarver.api.util :refer [wrap-response]])
-  (:require [dev.codecarver.api.article :refer [create_article]]))
+  (:require [dev.codecarver.api.routes.article :refer [article_routes]])
+  )
 
-(defroutes app
-           (->
-             (GET "/article" [] create_article)
-             (wrap-defaults api-defaults)
-             wrap-params
-             (wrap-routes wrap-response)
-             wrap-json-response)
-           (route/not-found "<h1>Page not found</h1>"))
+(def app
+  (routes
+    article_routes
+    (route/not-found "<h1>Page not found</h1>")))
 
-(defn -main []
-  (run-jetty app {:port 3000}))
-
-
-
-
-
+  (defn -main []
+    (run-jetty app {:port 3000}))
---
+++ src/dev/codecarver/api/routes/article.clj
@@ -0,0 +1,16 @@
+(ns dev.codecarver.api.routes.article
+  (:require [compojure.core :refer [defroutes wrap-routes GET]])
+  (:require [ring.middleware.defaults :refer [wrap-defaults api-defaults]])
+  (:require [ring.middleware.params :refer [wrap-params]])
+  (:require [ring.middleware.json :refer [wrap-json-response]])
+  (:require [dev.codecarver.api.util :refer [wrap-response]])
+  (:require [dev.codecarver.api.article :refer [create_article]]))
+
+
+(defroutes article_routes
+           (->
+             (GET "/article" [] create_article)
+             (wrap-defaults api-defaults)
+             wrap-params
+             (wrap-routes wrap-response)
+             wrap-json-response))
