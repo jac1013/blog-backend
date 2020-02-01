9554cd9d55f1217ac11f136c431d99f31650bf0c
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-01 18:15:46 -0500

    Fix details on schema SQL

--- src/dev/codecarver/repository/postgresql/schema.sql
+++ src/dev/codecarver/repository/postgresql/schema.sql
@@ -1,31 +1,31 @@
 CREATE TABLE blog_metadata (
     id SERIAL,
-    title varchar(200),
-    subtitle varchar(500),
+    title varchar(200) NOT NULL,
+    subtitle varchar(500) NOT NULL,
     copyright varchar (100),
     logo_credit varchar(200)
 )
 
 CREATE TABLE article (
-    id            SERIAL,
-    title         varchar(200),
-    body          varchar ,
-    created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    url           varchar(500),
+    id SERIAL,
+    title varchar(200) NOT NULL,
+    body varchar NOT NULL ,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    url varchar(500),
     repository_url varchar(500)
 );
 
 CREATE TABLE like (
     id SERIAL,
-    article_id INTEGER,
-    ip_address varchar(45),
+    article_id INTEGER NOT NULL,
+    ip_address varchar(45) NOT NULL,
     FOREIGN KEY (article_id) REFERENCES article (id)
 )
 
 CREATE TABLE user (
     id SERIAL,
-    username varchar(200),
-    password varchar
+    username varchar(200) NOT NULL,
+    password varchar NOT NULL
 )
 
