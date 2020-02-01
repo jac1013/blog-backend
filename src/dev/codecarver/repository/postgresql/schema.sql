29d4ce1bf5c646e10cc7a702b130366002bce997
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-01 18:13:54 -0500

    Fix syntax error in SQL schema

--- src/dev/codecarver/repository/postgresql/schema.sql
+++ src/dev/codecarver/repository/postgresql/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE blog_metadata (
-    id SERIAL
-    title varchar(200)
-    subtitle varchar(500)
-    copyright varchar (100)
+    id SERIAL,
+    title varchar(200),
+    subtitle varchar(500),
+    copyright varchar (100),
     logo_credit varchar(200)
 )
 
@@ -12,20 +12,20 @@ CREATE TABLE article (
     body          varchar ,
     created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    url           varchar(500)
+    url           varchar(500),
     repository_url varchar(500)
 );
 
 CREATE TABLE like (
-    id SERIAL
-    article_id INTEGER
-    ip_address varchar(45)
+    id SERIAL,
+    article_id INTEGER,
+    ip_address varchar(45),
     FOREIGN KEY (article_id) REFERENCES article (id)
 )
 
 CREATE TABLE user (
-    id SERIAL
-    username varchar(200)
+    id SERIAL,
+    username varchar(200),
     password varchar
 )
 
