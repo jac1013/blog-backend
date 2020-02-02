2f1c5709351eadc7a6a6a9e78b05d0891dfc8fcd
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-01 20:42:25 -0500

    Add env handling through environ

--- src/dev/codecarver/repository/postgresql/article.clj
+++ src/dev/codecarver/repository/postgresql/article.clj
@@ -1,18 +1,22 @@
 (ns dev.codecarver.repository.postgresql.article
+  (:require [environ.core :refer [env]])
   (:require [dev.codecarver.domain.repository.article :refer [ArticleRepository]]
             [clojure.java.jdbc :as sql]))
+
+(defn ^:private get_connection_url []
+  (env :db-url))
 
 (deftype ArticleRepoPostgreSQL []
   ArticleRepository
   (save! [_ article] (sql/insert!
-                       "postgresql://admin:admin@localhost:5432/blog"
+                       (get_connection_url)
                        :article article))
   (update! [_ article] (sql/update!
-                         "postgresql://admin:admin@localhost:5432/blog"
+                         (get_connection_url)
                          :article article ["id = ?" (:id article)]))
   (find [_ id] (sql/get-by-id
-                    "postgresql://admin:admin@localhost:5432/blog"
-                    :article id)))
+                 (get_connection_url)
+                 :article id)))
 
 (defn articleRepoPostgreSQL []
   (ArticleRepoPostgreSQL.))
