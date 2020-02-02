b881b6ab7c611432afe3e6502949f42fdea0397d
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-02 09:02:25 -0500

    Fix article construction in api controller

--- src/dev/codecarver/api/controllers/article.clj
+++ src/dev/codecarver/api/controllers/article.clj
@@ -1,10 +1,9 @@
 (ns dev.codecarver.api.controllers.article
-  (:require [dev.codecarver.domain.interactors.implementations.article :refer [articleInteractor]])
+  (:require [dev.codecarver.factory.article :refer [articleInteractor]])
   (:require [dev.codecarver.domain.interactors.article :refer [create]]))
 
 (defn create_article [request]
   {:status 200
    :headers {"Content-Type" "application/json"}
    :body (create (articleInteractor) {:title "I have more than ten characters"
-                                      :body "I'm just a body that's not empty"
-                                      :hello (get-in request [:params :ciao])})})
+                                      :body "I'm just a body that's not empty"})})
