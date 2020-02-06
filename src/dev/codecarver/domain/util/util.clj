a54297a0f8f9bfbf7b83a7d857afda26c4a0bad8
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-06 18:54:26 -0500

    Refactor map discovery through destructuring

--- src/dev/codecarver/domain/util/util.clj
+++ src/dev/codecarver/domain/util/util.clj
@@ -2,9 +2,7 @@
   (:require [validateur.validation :refer :all]))
 
 
-(defn validate [options] (let [action (get options :action)
-                               validator (get options :validator)
-                               to_validate (get options :to_validate)]
+(defn validate [options] (let [{:keys [validator to_validate action]} options]
                            (if (valid? validator to_validate)
                              (action)
                              (identity {:validation_error (validator to_validate)}))))
