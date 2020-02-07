40833adf2ad74749e475d995acd8cf709b18642a
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-06 19:04:08 -0500

    Refactor minor function movement

--- src/dev/codecarver/domain/entities/article.clj
+++ src/dev/codecarver/domain/entities/article.clj
@@ -4,12 +4,12 @@
                                            length-of]])
   (:require [dev.codecarver.domain.util.util :as util]))
 
+(defrecord Article [id title body created_at updated_at url repository_url is_publish article_id])
+
 (def ^:private validator (validation-set
                            (presence-of :title)
                            (presence-of :body)
                            (length-of :title :within (range 10 50))))
-
-(defrecord Article [id title body created_at updated_at url repository_url is_publish article_id])
 
 (defn validate [options]
   (util/validate (merge options {:validator validator})))
