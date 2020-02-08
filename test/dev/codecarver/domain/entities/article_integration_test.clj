3daa00fbbfb31f14315cb20ef663e57a2ce3308a
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-08 15:55:58 -0500

    Add integration test for create article

---
+++ test/dev/codecarver/domain/entities/article_integration_test.clj
@@ -0,0 +1,11 @@
+(ns dev.codecarver.domain.entities.article-integration-test
+  (:refer-clojure :exclude [update get list])
+  (:require [clojure.core :as c])
+  (:require [dev.codecarver.factory.article :refer [articleInteractor]])
+  (:require [dev.codecarver.domain.interactors.article_interactor :refer :all])
+  (:require [clojure.test :refer :all]))
+
+
+(deftest article-integration-test
+  (testing "Should be able to create an article"
+    (is true (= c/get (create (articleInteractor) {:title "longer than ten characters title" :body "He we have a body" :is_publish false}) :body "He we have a body"))))
