ba9e74d9266d6f72daa2c20a91fef3c355e40dfa
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-08 18:58:43 -0500

    Add randomness to tests

--- test/dev/codecarver/domain/entities/article_integration_test.clj
+++ test/dev/codecarver/domain/entities/article_integration_test.clj
@@ -3,8 +3,9 @@
   (:require [clojure.core :as c])
   (:require [dev.codecarver.factory.article :refer [articleInteractor]])
   (:require [dev.codecarver.domain.interactors.article_interactor :refer :all])
+  (:require [clojure.data.generators :as r])
   (:require [clojure.test :refer :all]))
 
 (deftest ^:integration article-integration-test
   (testing "Should be able to create an article"
-    (is true (= c/get (create (articleInteractor) {:title "longer than ten characters title" :body "He we have a body" :is_publish false}) :body "He we have a body"))))
+    (is true (= c/get (create (articleInteractor) {:title (r/string "a" (r/uniform 11 49)) :body (r/string "a" (r/uniform 50 500)) :is_publish false}) :id 1))))
