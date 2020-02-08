563b27a15e8a68283969952406a25724dba8158b
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-08 09:37:59 -0500

    Add test for article validation

---
+++ test/dev/codecarver/domain/entities/article_test.clj
@@ -0,0 +1,17 @@
+(ns dev.codecarver.domain.entities.article_test
+  (:require [clojure.test :refer :all]
+            [dev.codecarver.domain.entities.article :refer :all])
+  (:require [clojure.data.generators :as r]))
+
+(deftest article-test
+  (testing "Should return a map with error if title is greater than 50"
+    (is (= true (contains? (validate {:action #() :to_validate {:title (r/string "a" 51) :body (r/string "qwer" 100)}}) :validation_error))))
+  (testing "Should return a map with error if title is lower than 10"
+    (is (= true (contains? (validate {:action #() :to_validate {:title (r/string "a" 8) :body (r/string "qwer" 100)}}) :validation_error))))
+  (testing "Should return an empty function if title is between 10 and 50"
+    (is (= true (empty? (validate {:action #() :to_validate {:title (r/string "a" 25) :body (r/string "qwer" 100)}})))))
+  (testing "Should return a map with error if body is empty"
+    (is (= true (contains? (validate {:action #() :to_validate {:title (r/string "a" 25)}}) :validation_error))))
+  (testing "Should return a map with error if title is empty"
+    (is (= true (contains? (validate {:action #() :to_validate {:body (r/string "qwer" 100)}}) :validation_error))))
+  )
