ec069e94c5e4f550a62f45bfe1025c0a02f7a531
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-08 19:05:18 -0500

    Add test for get by id use case

--- test/dev/codecarver/domain/entities/article_integration_test.clj
+++ test/dev/codecarver/domain/entities/article_integration_test.clj
@@ -13,4 +13,6 @@
   (testing "Should be able to create an article"
     (is 1 (c/get (create (articleInteractor) {:title (random-string 11 49) :body (random-string 50 500) :is_publish false}) :id)))
   (testing "Should be able to update an article"
-    (is true (c/get (update (articleInteractor) {:title (random-string 11 49) :body (random-string 50 500) :is_publish true}) :is_publish))))
+    (is true (c/get (update (articleInteractor) {:title (random-string 11 49) :body (random-string 50 500) :is_publish true}) :is_publish)))
+  (testing "Should be able to get an article by ID"
+    (is true (c/get (get (articleInteractor) 1) :is_publish))))
