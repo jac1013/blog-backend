25ace288d45c9a073d3a302dd6b3f4b54e9784d9
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-09 16:17:35 -0500

    Fix tests issues

--- test/dev/codecarver/domain/entities/article_integration_test.clj
+++ test/dev/codecarver/domain/entities/article_integration_test.clj
@@ -9,18 +9,20 @@
 (defn random-string [low high]
   (r/string "a" (r/uniform low high)))
 
-(def article
-  (create (articleInteractor) {:title (random-string 11 49) :body (random-string 50 500) :is_publish true}))
+(defn article []
+  {:title (random-string 11 49) :body (random-string 50 500) :is_publish true})
 
 (deftest ^:integration article-integration-test
   (testing "Should be able to create an article"
-    (is (= true (c/get article :is_publish))))
+    (is (= true (c/get (create (articleInteractor) (article)) :is_publish))))
   (testing "Should be able to update an article"
-    (is (= false (c/get (update (articleInteractor) (merge article {:is_publish false})) :is_publish))))
+    (is (= false (c/get (update (articleInteractor) (merge (article) {:id 1 :is_publish false})) :is_publish))))
   (testing "Should be able to get an article by ID"
-    (is (= false (c/get (get (articleInteractor) (:id article)) :is_publish))))
+    (is (= false (c/get (get (articleInteractor) 1) :is_publish))))
   (testing "Should be able to know if an article is publish"
-    (is (= false (is_publish (articleInteractor) (:id article)))))
-  (testing "Should be able to know if an article is publish"
-    (is (= true (c/get (publish (articleInteractor) (:id article)) :is_publish))))
-  )
+    (is (= false (is_publish (articleInteractor) 1))))
+  (testing "Should be able to publish an article"
+    (is (= true (c/get (publish (articleInteractor) 1) :is_publish))))
+  (testing "Should be able to list articles"
+    (is (= 1 (count (filter (fn [value]
+                       (= (:id value) 2)) (list (articleInteractor))))))))
