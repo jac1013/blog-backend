8914f7e35cac4781dd5b0fdf966bf5cfd7015e5c
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-11 07:38:54 -0500

    Refactor interactor and add logging to it

--- src/dev/codecarver/domain/interactors/implementations/article_interactor.clj
+++ src/dev/codecarver/domain/interactors/implementations/article_interactor.clj
@@ -3,21 +3,54 @@
   (:require [clojure.core :as core])
   (:require [dev.codecarver.domain.entities.article :refer [validate]])
   (:require [dev.codecarver.domain.repository.article :as repo])
-  (:require [dev.codecarver.domain.interactors.article_interactor :refer [ArticleInteractor]]))
+  (:require [dev.codecarver.domain.interactors.article_interactor :refer [ArticleInteractor]])
+  (:require [taoensso.timbre :refer [error]])
+  (:require [clojure.core.strint :refer [<<]]))
+
+(defn ^:private save! [repo article]
+  (fn []
+    (try
+      (repo/save! repo article)
+      (catch Exception e
+        ((error
+           (<< "There was a problem creating an article \n ~{e}"))
+         (throw e))))))
+
+(defn ^:private update! [repo article]
+  (fn []
+    (try
+      (repo/update! repo article)
+      (catch Exception e
+        ((error (<< "There was a problem updating an article ~{article} \n ~{e}"))
+         (throw e))))))
+
+(defn ^:private find [repo id]
+  (try
+    (repo/find repo id)
+    (catch Exception e
+      ((error (<< "There was a problem finding an article \n ~{id} ~{e}"))
+       (throw e)))))
+
+(defn ^:private find-all [repo]
+  (try
+    (repo/find-all repo)
+    (catch Exception e
+      ((error (<< "There was a problem finding all articles \n ~{e}"))
+       (throw e)))))
 
 (deftype ArticleInteractorImpl [repository]
   ArticleInteractor
   (create
     [_ article]
-    (validate {:action      (fn [] (repo/save! repository article))
+    (validate {:action      (save! repository article)
                :to_validate article}))
   (update
     [_ article]
-    (validate {:action      (fn [] (repo/update! repository article))
+    (validate {:action      (update! repository article)
                :to_validate article}))
   (get
     [_ id]
-    (repo/find repository id))
+    (find repository id))
   (is_publish
     [this id]
     (boolean (core/get (.get this id) :is_publish)))
@@ -25,4 +58,4 @@
     [this id]
     (.update this
              (assoc (.get this id) :is_publish true :url "this is a generated url")))
-  (list [_] (repo/find-all repository)))
+  (list [_] (find-all repository)))
