4a2ce38ee52b9f38ec9d33eadda53b56a73beac4
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-12 19:24:49 -0500

    Add route for PUT controller

--- src/dev/codecarver/api/routes/article.clj
+++ src/dev/codecarver/api/routes/article.clj
@@ -1,5 +1,5 @@
 (ns dev.codecarver.api.routes.article
-  (:require [compojure.core :refer [defroutes wrap-routes GET POST]])
+  (:require [compojure.core :refer [defroutes wrap-routes GET POST PUT]])
   (:require [ring.middleware.defaults :refer [wrap-defaults api-defaults]])
   (:require [ring.middleware.params :refer [wrap-params]])
   (:require [ring.middleware.json :refer [wrap-json-response wrap-json-body]])
@@ -15,6 +15,12 @@
    wrap-json-response)
   (->
    (POST "/article" [] create_article)
+   (wrap-defaults api-defaults)
+   wrap-json-body
+   (wrap-routes wrap-response)
+   wrap-json-response)
+  (->
+   (PUT "article/:id" [] create_article)
    (wrap-defaults api-defaults)
    wrap-json-body
    (wrap-routes wrap-response)
