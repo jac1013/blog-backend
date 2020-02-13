cba2eba39a1a18599b319c1cc09fed667a52070e
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-12 19:11:43 -0500

    Add real GET endpoint controller

--- src/dev/codecarver/api/controllers/article.clj
+++ src/dev/codecarver/api/controllers/article.clj
@@ -1,9 +1,11 @@
 (ns dev.codecarver.api.controllers.article
+  (:refer-clojure :exclude [update find get list])
   (:require [dev.codecarver.factory.article :refer [articleInteractor]])
   (:require [dev.codecarver.api.util :refer [json-response]])
   (:require [taoensso.timbre :refer [warn]])
   (:require [clojure.core.strint :refer [<<]])
-  (:require [dev.codecarver.domain.interactors.article_interactor :refer [create]]))
+  (:require [dev.codecarver.domain.interactors.article_interactor :refer [create
+                                                                          get]]))
 
 (defn create_article [request]
   {:status 200
@@ -14,12 +16,20 @@
 (defn ^:private body-to-article [request]
   {:title (get-in request [:body "title"]) :body (get-in request [:body "body"])})
 
-(defn create_article_ [request]
+(defn create_article [request]
   (try
-   (let [article (create (articleInteractor) (body-to-article request)) response (json-response)]
-     (if (contains? article :validation_error)
-       (assoc response :status 400 :body article)
-       (assoc response :body article)))
-   (catch Exception e
-     (warn (<< "create_article request failed \n ~{e}"))
-     (assoc (json-response) :status 500))))
+    (let [article (create (articleInteractor) (body-to-article request)) response (json-response)]
+      (if (contains? article :validation_error)
+        (assoc response :status 400 :body article)
+        (assoc response :body article)))
+    (catch Exception e
+      (warn (<< "create_article request failed \n ~{e}"))
+      (assoc (json-response) :status 500))))
+
+(defn get_article [request]
+  (try
+    (let [article (get (articleInteractor) (Integer/parseInt (get-in request [:params :id]))) response (json-response)]
+      (assoc response :body article))
+    (catch Exception e
+      (warn (<< "get_article request failed \n ~{e}"))
+      (assoc (json-response) :status 500))))
--- src/dev/codecarver/api/routes/article.clj
+++ src/dev/codecarver/api/routes/article.clj
@@ -4,18 +4,18 @@
   (:require [ring.middleware.params :refer [wrap-params]])
   (:require [ring.middleware.json :refer [wrap-json-response wrap-json-body]])
   (:require [dev.codecarver.api.util :refer [wrap-response]])
-  (:require [dev.codecarver.api.controllers.article :refer [create_article create_article_]]))
+  (:require [dev.codecarver.api.controllers.article :refer [get_article create_article]]))
 
 (defroutes article_routes
-           (->
-             (GET "/article" [] create_article)
-             (wrap-defaults api-defaults)
-             wrap-params
-             (wrap-routes wrap-response)
-             wrap-json-response)
-           (->
-             (POST "/article" [] create_article_)
-             (wrap-defaults api-defaults)
-             wrap-json-body
-             (wrap-routes wrap-response)
-             wrap-json-response))
+  (->
+   (GET "/article/:id" [] get_article)
+   (wrap-defaults api-defaults)
+   wrap-params
+   (wrap-routes wrap-response)
+   wrap-json-response)
+  (->
+   (POST "/article" [] create_article)
+   (wrap-defaults api-defaults)
+   wrap-json-body
+   (wrap-routes wrap-response)
+   wrap-json-response))
