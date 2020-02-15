b6f6354e185cfc8a4293f390d3b56a92e4ea1af8
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-15 12:50:10 -0500

    Fix usage of rsync for scp, the former is not in the CI box

--- deployment/publish_to_aws.sh
+++ deployment/publish_to_aws.sh
@@ -3,7 +3,7 @@
 echo "Removing old jar..."
 ssh $AWS_HOST "rm dev.codecarver*-standalone.jar"
 echo "Uploading new jar to AWS..."
-rsync -avr ./target/uberjar/dev.codecarver*-standalone.jar $AWS_HOST:~/
+scp ./target/uberjar/dev.codecarver*-standalone.jar $AWS_HOST:~/
 echo "Running jar in AWS..."
 pwd
 ssh $AWS_HOST 'bash -s' < deployment/deploy.sh
