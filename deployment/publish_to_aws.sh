f51f7e7f2f5f340b7171b00133b8aa7eebca8705
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-02-15 13:32:14 -0500

    Fix typo on the options of ssh

--- deployment/publish_to_aws.sh
+++ deployment/publish_to_aws.sh
@@ -2,7 +2,7 @@
 
 echo "Removing old jar..."
 ## This is not secure apparently, I guess we should really do a mechanism where we pull from AWS instead of push from here.
-ssh StrictHostKeyChecking=no $AWS_HOST "rm dev.codecarver*-standalone.jar"
+ssh -o StrictHostKeyChecking=no $AWS_HOST "rm dev.codecarver*-standalone.jar"
 echo "Uploading new jar to AWS..."
 scp ./target/uberjar/dev.codecarver*-standalone.jar $AWS_HOST:~/
 echo "Running jar in AWS..."
