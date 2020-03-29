b2463f8ec4ec888380d3142c09d13bb1a4bea170
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-03-29 10:31:31 -0500

    Add implementation for like repo

---
+++ src/dev/codecarver/repository/postgresql/like.clj
@@ -0,0 +1,17 @@
+(ns dev.codecarver.repository.postgresql.like
+  (:require [dev.codecarver.repository.postgresql.db-config-connection :refer [db]])
+  (:require [dev.codecarver.domain.repository.like :refer [LikeRepository]]
+            [clojure.java.jdbc :as jdbc]))
+
+(deftype ^:private LikeRepo []
+  LikeRepository
+  (create! [_ article_id ip_address]
+    first (jdbc/insert!
+            db
+            :plus_one {:article_id article_id :ip_address ip_address}))
+  (remove! [_ id]
+    (jdbc/delete! db :plus_one ["id = ?" id])))
+
+
+(defn likeRepoPostgreSQL []
+  (LikeRepo.))
