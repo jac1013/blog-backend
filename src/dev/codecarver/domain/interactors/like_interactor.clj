d06dad75e64e7398fba54ebd9c1d28717e79cf69
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-03-29 10:42:52 -0500

    Add Like interactor

---
+++ src/dev/codecarver/domain/interactors/like_interactor.clj
@@ -0,0 +1,6 @@
+(ns dev.codecarver.domain.interactors.like-interactor)
+
+(defprotocol LikeInteractor
+  "Handle like/plus_one operations in the system"
+  (like! [_ like] "Creates a like/plus_one")
+  (unlike! [_ id] "Deletes a like/plus_one by ID"))
