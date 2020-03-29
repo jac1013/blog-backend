246d2c3c6ccb4f25c482360cc92cccd641c1ce16
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-03-29 10:15:38 -0500

    Remove CRUD operations repo, we don't need it

--- src/dev/codecarver/domain/repository/crud.clj
+++
@@ -1,9 +0,0 @@
-(ns dev.codecarver.domain.repository.crud)
-
-(defprotocol CrudOperations
-  "Basic Create/Read/Update/Delete"
-  (save! [_ model] "Saves an article")
-  (update! [_ model] "Modifies an article")
-  (find [_ id] "Finds an article by ID")
-  (delete! [_ id] "Finds an article by ID")
-  (find-all  [_] "Finds all articles"))
