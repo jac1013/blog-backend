609908de18220a49ed9f19535825ac59448c8ba1
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-06-20 19:59:24 -0500

    Fix to compact middleware usage in core.clj

--- src/dev/codecarver/api/core.clj
+++ src/dev/codecarver/api/core.clj
@@ -1,25 +1,18 @@
 (ns dev.codecarver.api.core
   (:gen-class)
   (:use ring.adapter.jetty)
-  (:require [compojure.core :refer [defroutes wrap-routes GET routes]]
+  (:require [compojure.core :refer [wrap-routes routes]]
             [compojure.route :as route])
   (:require [dev.codecarver.api.util :refer [wrap-response]])
   (:require [ring.middleware.json :refer [wrap-json-response]])
   (:require [ring.middleware.defaults :refer [wrap-defaults api-defaults]])
-  (:require [dev.codecarver.api.routes.article :refer [article-routes]])
-  (:require [dev.codecarver.api.routes.like :refer [like-routes]])
   (:require [dev.codecarver.util.env :refer [get-env]])
-  (:require [ring.middleware.cors :refer [wrap-cors]]))
+  (:require [ring.middleware.cors :refer [wrap-cors]])
+  (:require [dev.codecarver.api.routes.app :refer [app-routes]]))
 
 (def app
   (routes
-   (-> article-routes
-       (wrap-defaults api-defaults)
-       (wrap-response)
-       (wrap-json-response)
-       (wrap-cors :access-control-allow-origin [#"http://localhost:3000" #"https://codecarver.dev"]
-                  :access-control-allow-methods [:get :put :post :delete]))
-   (-> like-routes
+   (-> app-routes
        (wrap-defaults api-defaults)
        (wrap-response)
        (wrap-json-response)
---
+++ src/dev/codecarver/api/routes/app.clj
@@ -0,0 +1,8 @@
+(ns dev.codecarver.api.routes.app
+  (:require [compojure.core :refer [defroutes routes wrap-routes GET POST PUT]])
+  (:require [dev.codecarver.api.routes.article :refer [article-routes]])
+  (:require [dev.codecarver.api.routes.like :refer [like-routes]]))
+
+(defroutes app-routes
+           article-routes
+           like-routes)
